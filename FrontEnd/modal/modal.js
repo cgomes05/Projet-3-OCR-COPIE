9cb2efd8dd9e8fae9c66c5094c1d9bcfabe11600
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-04-06 13:09:25 +0200

    Fonctionnalité select pour les catégorie

--- FrontEnd/modal/modal.js
+++ FrontEnd/modal/modal.js
@@ -81,6 +81,7 @@ const btnModal1 = modal1.querySelector("#btn-modal1");
 // Récupération du bouton "Supprimer la galerie"
 const btnModal2 = modal1.querySelector("#btn-modal2");
 
+
 // Ajout de l'écouteur d'événement click sur le bouton "Ajouter une photo" , pour que l'utilisateur puisse ajouter une photo depuis son pc
 btnModal1.addEventListener("click", function () {
     // Modification du titre de la modal
@@ -93,85 +94,38 @@ btnModal1.addEventListener("click", function () {
     // Masquage des boutons "Ajouter une photo" et "Supprimer la galerie"
     btnModal1.style.display = "none";
     btnModal2.style.display = "none";
+    // Récupération de la div qui contient le forumlaire d'envoi 
+    const divForm = document.getElementById("divModalForm")
+    // Changement de style pour faire apparaitre la div dans la modal
+    divForm.style.display = "block";
 
-    // Création des éléments HTML nécessaires pour l'ajout d'une photo
-    const div = document.createElement("div");
-    div.classList.add("div-modal-style");
+    // Attribution des numéro d'id pour chaque catégorie dans la select
+    const categories = {
+        appart: 1,
+        objets: 2,
+        hotelresto: 3
+    };
 
-    const imgI = document.createElement("span");
-    imgI.classList.add("fa-sharp", "fa-regular", "fa-image");
-    imgI.classList.add("imgI-style");
+    // Récupération de l'élément en question afin de lui attribué la fonction 
+    // qui permettra de changer l'option en chiffre 
+    const selection = document.getElementById('select-categorie-style');
 
-    const boutonAjout = document.createElement("button");
-    boutonAjout.textContent = "+ Ajouter photo";
-
-    // Création d'un input de type "file"
-    const inputPhoto = document.createElement("input");
-    inputPhoto.type = "file";
-    inputPhoto.id = "inputPhotoId"
-
-
-
-    // Ajout de l'écouteur d'événement change sur l'input "file"
-    inputPhoto.addEventListener("change", function () {
-        // Récupération du fichier sélectionné
-        const fichier = inputPhoto.files[0];
-
-
-        // Vérification que le fichier est une image
-        if (fichier && fichier.type.startsWith("image/")) {
-            // Création d'un élément img pour afficher l'image
-            const image = new Image();
-            image.src = URL.createObjectURL(fichier);
-            image.alt = fichier.name;
-
-            // Ajout des propriétés width et height pour ajuster la taille de l'image
-            image.onload = function () {
-                const ratio = image.naturalWidth / image.naturalHeight;
-                if (ratio > 1) {
-                    image.width = 129;
-                    image.height = 129 / ratio;
-                    image.style.paddingBottom = "10px"
-                } else {
-                    image.height = 193;
-                    image.width = 193 * ratio;
-                    image.style.paddingBottom = "10px"
-                }
-            };
-
-            // Remplacement du logo "imgI" par l'image
-            imgI.parentNode.replaceChild(image, imgI);
-        } else {
-            alert("Veuillez sélectionner une image.");
-        }
+    selection.addEventListener('change', function () {
+        const selectionIndex = selection.selectedIndex;
+        const selectionOption = selection.options[selectionIndex];
+        const selectionCategorie = selectionOption.value;
+        const categorieId = categories[selectionCategorie]
+        console.log(categorieId);
     });
 
 
 
-    // Clic sur l'input "file"
-    boutonAjout.addEventListener("click", function () {
-        inputPhoto.click();
-    });
 
-    // Création des éléments pour le formulaire d'envois de la modal
-    const sousTitre = document.createElement("p");
-    sousTitre.textContent = "jpg, png : 4mo max";
 
-    // Ajout des élements titre et catégorie selon les consignes demandées
-    const inputTitre = document.createElement("input");
-    inputTitre.type = "text";
 
-    const labelTitre = document.createElement("label");
-    labelTitre.textContent = "Titre";
 
-    const inputCategorie = document.createElement("input");
-    inputCategorie.type = "number";
 
-    const labelCategorie = document.createElement("label");
-    labelCategorie.textContent = "Catégorie";
 
-    const btnValider = document.createElement("button");
-    btnValider.textContent = "Valider";
 
     // Ajout de la flèche retour avec la balise <i> de fontawesome
     const flecheRetour = document.createElement("i");
@@ -181,78 +135,6 @@ btnModal1.addEventListener("click", function () {
 
     // Ajout des éléments HTML créés à la modal
     title.appendChild(flecheRetour);
-
-    div.appendChild(imgI);
-    div.appendChild(boutonAjout);
-    div.appendChild(sousTitre);
-    div.appendChild(inputTitre);
-    div.appendChild(labelTitre);
-    div.appendChild(labelCategorie);
-    div.appendChild(inputCategorie);
-    div.appendChild(btnValider);
-    div.appendChild(inputPhoto);
-
-    modif.appendChild(div);
-
-    // Ajout des classes des éléments crées
-    boutonAjout.id = "bouton-ajout";
-    btnValider.id = "btn-valider-style";
-    sousTitre.id = "sous-titre-btnmodal";
-    inputTitre.id = "input-titre-style";
-    inputCategorie.id = "input-categorie-style";
-    labelTitre.id = "label-titre-style";
-    labelCategorie.id = "label-categorie-style";
-
-    console.log(boutonAjout);
-
-    // Ajout de l'écouteur d'évènement au click sur le bouton 'Valider"
-    btnValider.addEventListener("click", async () => {
-        //Récupération des données du formulaire
-
-
-        const inputPhotoForm = inputPhoto.files;
-        console.log(inputPhotoForm);
-
-
-        const titreFormulaire = inputTitre.value;
-        const categorieFormulaire = inputCategorie.value;
-
-        const test = document.getElementById('input-categorie-style').value;
-        console.log(test);
-
-
-        //Création de l'objet formData contenant les donnée du forumlaire
-        const donneeForm = new FormData();
-        donneeForm.append("image", inputPhotoForm);
-        donneeForm.append("title", titreFormulaire);
-        donneeForm.append("category", categorieFormulaire);
-
-        console.log("le formdata " + donneeForm);
-        const token = sessionStorage.getItem("token");
-        console.log(token);
-
-        const reponse = await fetch("http://localhost:5678/api/works", {
-            method: "POST",
-            headers: {
-                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY4MDU5MTgxMywiZXhwIjoxNjgwNjc4MjEzfQ.ZkF2SszKnE0cA3-G-mMbsJPliqzEFk3BTYy9ZgDoUUc",
-                accept: "application/json",
-                "Content-Type": "multipart/form-data"
-
-            },
-            body: JSON.stringify(donneeForm),
-        });
-
-        if (reponse.ok) {
-            // Récupere les travaux mis a jour si c'est ok 
-            const travaux = await fetch("http://localhost:5678/api/works");
-            const works = await travaux.json();
-            // Regénération de la gallery
-            genererworks(works);
-            genererworksmodal(works);
-        }
-    }
-    );
-
     // Ajout de l'écouteur d'événement click sur la flèche retour
     flecheRetour.addEventListener("click", function () {
         // Affichage de la galerie
@@ -261,9 +143,8 @@ btnModal1.addEventListener("click", function () {
         // Affichage des boutons "Ajouter une photo" et "Supprimer la galerie"
         btnModal1.style.display = "";
         btnModal2.style.display = "";
+        divForm.style.display = "none";
         title.textContent = "Gallerie photo";
 
-        // Suppression des éléments HTML créés pour l'ajout d'une photo
-        modif.removeChild(div);
     });
 });
